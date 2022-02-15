b83d85606d00875e6347c5cbd9d6b0a917763e10
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2022-02-15 15:45:44 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_lab2.3/Main.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_lab2._3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> fInstColumns = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Columns)
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            TaskDialog.Show("Количество колонн", fInstColumns.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
